9b8594d3c0756ec465d9e95fe38a4d1a5fbafede
Chelsea Baird <2886700b@student.gla.ac.uk>
2025-02-07 16:26:12 +0000

    Chapter 4

--- rango/views.py
+++ rango/views.py
@@ -6,3 +6,8 @@ def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
 
     return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'boldmessage': 'This tutorial has been put together by Chelsea!'}
+
+    return render(request, 'rango/about.html', context=context_dict)
